fd00ea0c62a2d3ce61e7a7e9b91b018b16a99115
Luong Son <sonluong165@gmail.com>
2024-02-26 09:03:47 +0700

    add tile

---
+++ src/tile/Tile.java
@@ -0,0 +1,13 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+import core.Position;
+import core.Size;
+
+public class Tile {
+    protected BufferedImage image;
+    protected Position pos;
+    protected Size sz;
+    protected boolean collition; 
+}
---
+++ src/tile/TileManager.java
@@ -0,0 +1,11 @@
+package tile;
+
+public class TileManager {
+    private int mapTileNum[][];
+    private Tile tile[];
+
+    TileManager()
+    {
+
+    }
+}
