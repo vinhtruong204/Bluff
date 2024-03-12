5ee29792b827bcd7077cfd4910a018b5636f2653
Luong Son <sonluong165@gmail.com>
2024-03-12 17:05:29 +0700

    update checktile player

--- src/playing/entity/Player.java
+++ src/playing/entity/Player.java
@@ -46,7 +46,7 @@ public class Player extends GameObject {
         animations = new BufferedImage[6][26];
         // box of player
         hitBox = new Rectangle(8, 8, 48, 48);
-        keyPressed = CheckKeyPress.keyDefault;
+        keyPressed = CheckKeyPress.Down;
         startAni = PlayerAnimationType.IDLE;
         this.TileMapNum = TileMapNum;
         onGround = false;
@@ -90,96 +90,92 @@ public class Player extends GameObject {
 
     private void upDatePosition() {
 
-        // if (!checkTile()) {
-        // if (keyPressed == CheckKeyPress.Up) {
-        //     velocity.setY(-speedY);
-        //     velocity.setX(0f);
-        //     onGround = false;
-        // } else if (keyPressed == CheckKeyPress.Down) {
-        //     velocity.setY(speedY);
-        //     velocity.setX(0f);
-        if (keyPressed == CheckKeyPress.Right && keyPressed != CheckKeyPress.Left) {
-            if (onGround) {
+        if (!checkTile()) {
+            if (keyPressed == CheckKeyPress.Up) {
+                velocity.setY(-speedY);
+                velocity.setX(0f);
+                onGround = false;
+            } else if (keyPressed == CheckKeyPress.Down) {
+                velocity.setY(speedY);
+                velocity.setX(0f);
+            } else if (keyPressed == CheckKeyPress.Right && keyPressed != CheckKeyPress.Left) {
+                if (onGround) {
+                    velocity.setX(speedX);
+                    velocity.setY(0f);
+                } else {
+                    velocity.setX(speedX);
+                    velocity.setY(speedY);
+                }
                 velocity.setX(speedX);
-                velocity.setY(0f);
-            } else {
-                // velocity.setX(speedX);
-                // velocity.setY(speedY);
-            }
-            velocity.setX(speedX);
 
-        } else if (keyPressed == CheckKeyPress.Left && keyPressed != CheckKeyPress.Right) {
-            if (onGround) {
-                velocity.setX(-speedX);
-                velocity.setY(0f);
-            } else {
-                // velocity.setX(-speedX);
-                // velocity.setY(speedY);
+            } else if (keyPressed == CheckKeyPress.Left && keyPressed != CheckKeyPress.Right) {
+                if (onGround) {
+                    velocity.setX(-speedX);
+                    velocity.setY(0f);
+                } else {
+                    velocity.setX(-speedX);
+                    velocity.setY(speedY);
+                }
             }
+            position.setY(position.getY() + velocity.getY());
+            position.setX(position.getX() + velocity.getX());
         }
-        position.setY(position.getY() + velocity.getY());
-        position.setX(position.getX() + velocity.getX());
-        // }
     }
 
     private boolean checkTile() {
-    int entityLeftWorldX = (int) position.getX() + hitBox.x;
-    int entityRightWorldX = (int) position.getX() + hitBox.x + hitBox.width;
-    int entityTopWorldY = (int) position.getY() + hitBox.y;
-    int entityBottomWorldY = (int) position.getY() + hitBox.y + hitBox.height;
+        int entityLeftWorldX = (int) position.getX() + hitBox.x;
+        int entityRightWorldX = (int) position.getX() + hitBox.x + hitBox.width;
+        int entityTopWorldY = (int) position.getY() + hitBox.y;
+        int entityBottomWorldY = (int) position.getY() + hitBox.y + hitBox.height;
 
-    int entityLeftCol = entityLeftWorldX / Tile.TILE_SIZE;
-    int entityRightCol = entityRightWorldX / Tile.TILE_SIZE;
-    int entityTopRow = entityTopWorldY / Tile.TILE_SIZE;
-    int entityBottomRow = entityBottomWorldY / Tile.TILE_SIZE;
+        int entityLeftCol = entityLeftWorldX / Tile.TILE_SIZE;
+        int entityRightCol = entityRightWorldX / Tile.TILE_SIZE;
+        int entityTopRow = entityTopWorldY / Tile.TILE_SIZE;
+        int entityBottomRow = entityBottomWorldY / Tile.TILE_SIZE;
 
-    int tileNum1, tileNum2;
-    boolean flag = false;
+        int tileNum1, tileNum2;
+        boolean flag = false;
 
-    switch (keyPressed) {
-    case CheckKeyPress.Up: {
-    entityTopRow = (entityTopWorldY - (int) speedY) / Tile.TILE_SIZE;
-    tileNum1 = TileMapNum[entityLeftCol][entityTopRow];
-    tileNum2 = TileMapNum[entityRightCol][entityTopRow];
-    if (tile[tileNum1].getCollition() == true || tile[tileNum2].getCollition() ==
-    true) {
-    flag = true;
-    }
-    break;
-    }
-    case CheckKeyPress.Down: {
-    entityBottomRow = (entityBottomWorldY + (int) speedY) / Tile.TILE_SIZE;
-    tileNum1 = TileMapNum[entityLeftCol][entityBottomRow];
-    tileNum2 = TileMapNum[entityRightCol][entityBottomRow];
-    if (tile[tileNum1].getCollition() == true || tile[tileNum2].getCollition() ==
-    true) {
-    flag = true;
-    onGround = true;
-    }
-    break;
-    }
-    case CheckKeyPress.Left: {
-    entityLeftCol = (entityLeftWorldX - (int) speedX) / Tile.TILE_SIZE;
-    tileNum1 = TileMapNum[entityLeftCol][entityTopRow];
-    tileNum2 = TileMapNum[entityLeftCol][entityBottomRow];
-    if (tile[tileNum1].getCollition() == true || tile[tileNum2].getCollition() ==
-    true) {
-    flag = true;
-    }
-    break;
-    }
-    case CheckKeyPress.Right: {
-    entityRightCol = (entityRightWorldX + (int) speedX) / Tile.TILE_SIZE;
-    tileNum1 = TileMapNum[entityRightCol][entityTopRow];
-    tileNum2 = TileMapNum[entityRightCol][entityBottomRow];
-    if (tile[tileNum1].getCollition() == true || tile[tileNum2].getCollition() ==
-    true) {
-    flag = true;
-    }
-    break;
-    }
-    }
-    return flag;
+        switch (keyPressed) {
+            case CheckKeyPress.Up: {
+                entityTopRow = (entityTopWorldY - (int) speedY) / Tile.TILE_SIZE;
+                tileNum1 = TileMapNum[entityLeftCol][entityTopRow];
+                tileNum2 = TileMapNum[entityRightCol][entityTopRow];
+                if (tileNum1 != 0 || tileNum2 != 0) {
+                    flag = true;
+                }
+                break;
+            }
+            case CheckKeyPress.Down: {
+                entityBottomRow = (entityBottomWorldY + (int) speedY) / Tile.TILE_SIZE;
+                tileNum1 = TileMapNum[entityLeftCol][entityBottomRow];
+                tileNum2 = TileMapNum[entityRightCol][entityBottomRow];
+                if (tileNum1 != 0 || tileNum2 != 0) {
+                    flag = true;
+                    onGround = true;
+                }
+                break;
+            }
+            case CheckKeyPress.Left: {
+                entityLeftCol = (entityLeftWorldX - (int) speedX) / Tile.TILE_SIZE;
+                tileNum1 = TileMapNum[entityLeftCol][entityTopRow];
+                tileNum2 = TileMapNum[entityLeftCol][entityBottomRow];
+                if (tileNum1 != 0 || tileNum2 != 0) {
+                    flag = true;
+                }
+                break;
+            }
+            case CheckKeyPress.Right: {
+                entityRightCol = (entityRightWorldX + (int) speedX) / Tile.TILE_SIZE;
+                tileNum1 = TileMapNum[entityRightCol][entityTopRow];
+                tileNum2 = TileMapNum[entityRightCol][entityBottomRow];
+                if (tileNum1 != 0 || tileNum2 != 0) {
+                    flag = true;
+                }
+                break;
+            }
+        }
+        return flag;
     }
 
     public void setAniType() {
